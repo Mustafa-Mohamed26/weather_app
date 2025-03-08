84a916a0e130b252ee0b8242df809ceca43be5b5
Mustafa-Mohamed26 <mustafamohameda447@gmail.com>
2025-03-08 13:29:52 +0200

    remove the database

--- lib/Providers/weather_provider.dart
+++ lib/Providers/weather_provider.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:geocoding/geocoding.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:weather_app/core/db_helper.dart';
 import 'package:weather_app/core/service/weather_service.dart';
 import '../models/weather_model.dart';
 
@@ -14,58 +11,19 @@ class WeatherProvider extends ChangeNotifier {
   WeatherModel? get weather => _weather;
   bool get isLoading => _isLoading;
 
-  WeatherProvider() {
-    _loadWeatherFromDB(); // Load stored weather on startup
-  }
+  WeatherProvider();
 
-  Future<void> _loadWeatherFromDB() async {
-    _weather = await WeatherDatabase.instance.getWeather();
-    notifyListeners();
-  }
-  
   Future<void> fetchWeather(String city) async {
     _isLoading = true;
     notifyListeners();
 
     try {
       _weather = await _weatherService.fetchWeather(city);
-      // await WeatherDatabase.instance.deleteWeather(); // Clear old data
-      // await WeatherDatabase.instance.insertWeather(_weather!); // Save new data
     } catch (e) {
-      _weather = await WeatherDatabase.instance.getWeather(); // Load from database if fetch fails
+      _weather = null; // Clear weather data on failure
     }
 
     _isLoading = false;
     notifyListeners();
-  }
-
-  
-
-  // 🔹 Fetch weather by GPS
-  Future<void> getCurrentLocationWeather() async {
-    try {
-      LocationPermission permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied ||
-          permission == LocationPermission.deniedForever) {
-        print("Location permission denied.");
-        return;
-      }
-
-      Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high,
-      );
-
-      List<Placemark> placemarks = await placemarkFromCoordinates(
-        position.latitude,
-        position.longitude,
-      );
-
-      if (placemarks.isNotEmpty) {
-        String cityName = placemarks.first.locality ?? "Unknown";
-        await fetchWeather(cityName);
-      }
-    } catch (e) {
-      print("Error getting location: $e");
-    }
   }
 }
