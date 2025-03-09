4774250c906378da6b4fc89b84baa977db61f61c
Mustafa-Mohamed26 <mustafamohameda447@gmail.com>
2025-03-09 03:05:33 +0200

    start search screen

--- lib/Pages/home/search_screen.dart
+++ lib/Pages/home/search_screen.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/widgets/custom_app_bar.dart';
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Search Weather")),
+      appBar: CustomAppBar(title: "Pick location"),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        
+        child: Text("Find the area or city that you want to know the detailed weather info at this time"),
       ),
     );
   }
