ba58cadf75e1d06704104e42ecb095c7544f8e95
Mustafa-Mohamed26 <mustafamohameda447@gmail.com>
2025-03-12 02:31:31 +0200

    modifying the weather card

--- lib/Pages/home/search_screen.dart
+++ lib/Pages/home/search_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:weather_app/widgets/weather_card.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -9,13 +9,12 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
-  
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      
+      body: Container(
+        child: WeatherCard(city: "Cairo"),
+      ),
     );
   }
 }
--- lib/widgets/weather_card.dart
+++ lib/widgets/weather_card.dart
@@ -1,21 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:weather_app/Providers/weather_provider.dart';
 
-class WeatherCard extends StatelessWidget {
+class WeatherCard extends StatefulWidget {
   final String city;
-  final String icon;
-  final int temperature;
-  final String description;
 
-  const WeatherCard({
-    super.key,
-    required this.city,
-    required this.icon,
-    required this.temperature,
-    required this.description,
-  });
+  const WeatherCard({super.key, required this.city});
+
+  @override
+  _WeatherCardState createState() => _WeatherCardState();
+}
+
+class _WeatherCardState extends State<WeatherCard> {
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchWeather();
+  }
+
+  Future<void> _fetchWeather() async {
+    final weatherProvider =
+        Provider.of<WeatherProvider>(context, listen: false);
+    await weatherProvider.fetchWeather(widget.city);
+    setState(() {
+      _isLoading = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    final weatherProvider = Provider.of<WeatherProvider>(context);
+    final weather = weatherProvider.weather;
+
+    if (_isLoading || weather == null) {
+      return const Center(child: CircularProgressIndicator());
+    }
+
+    final String cityName = weather.city ?? "Unknown City";
+    final String? iconCode = weather.icon;
+    final String weatherIconUrl = iconCode != null
+        ? "https://openweathermap.org/img/w/$iconCode.png"
+        : "";
+
     return Card(
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
       elevation: 4,
@@ -25,25 +53,26 @@ class WeatherCard extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              city,
+              cityName,
               style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 5),
-            if (icon.isNotEmpty)
+            if (iconCode != null)
               Image.network(
-                "https://openweathermap.org/img/w/$icon.png",
+                weatherIconUrl,
                 width: 60,
                 height: 60,
-                errorBuilder: (context, error, stackTrace) => const Icon(Icons.error, size: 50, color: Colors.red),
+                errorBuilder: (context, error, stackTrace) =>
+                    const Icon(Icons.error, size: 50, color: Colors.red),
               ),
             const SizedBox(height: 5),
             Text(
-              "$temperature°C",
+              "${weather.temperature}°C",
               style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 5),
             Text(
-              description,
+              weather.description ?? "",
               style: const TextStyle(fontSize: 14, color: Colors.grey),
               textAlign: TextAlign.center,
             ),
